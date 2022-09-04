18d5618dcc1f9dd97719151c10095c3de55b622f
PriyankaBharathiLN <93275628+PriyankaBharathiLN@users.noreply.github.com>
2022-09-04 13:02:50 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,2 @@
+import numpy as np
+print("hello this is pika..as i have already mentioned before this is my first time here creating a repository...i hope this goes well  make sure to check out my simple projects "
